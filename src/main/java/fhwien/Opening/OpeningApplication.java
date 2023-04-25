f405dd490bf6d58f91e043295728625699449c2f
Tayyib <ic20b101@technikum-wien.at>
2023-04-25 17:59:25 +0200

    Initial commit

---
+++ src/main/java/fhwien/Opening/OpeningApplication.java
@@ -0,0 +1,13 @@
+package fhwien.Opening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpeningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpeningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fhwien/Opening/OpeningApplicationTests.java
@@ -0,0 +1,13 @@
+package fhwien.Opening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpeningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
